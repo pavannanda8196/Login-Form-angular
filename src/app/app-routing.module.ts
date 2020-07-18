ffbf3813efae4c7a53c7ee643fbdb4ce6a52edfb
pavannanda8196 <64827007+pavannanda8196@users.noreply.github.com>
2020-07-18 09:05:39 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+  { path: 'contact', component: ContactComponent },
+
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
